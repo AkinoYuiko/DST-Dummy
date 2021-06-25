17833458c654612d386437b93f64f89856a49569
秋一 <civi@rcg.fun>
2021-06-26 00:41:19 +0800

    fixed bigportaits issue.

--- modinfo.lua
+++ modinfo.lua
@@ -1,10 +1,10 @@
-version = "1.10.1"
+version = "1.10.2"
 -- basic info --
 name = locale == "zh" and "梦魇少女 达米" or "Dummy"
 author = locale == "zh" and "丁香女子学校" or "Civi, 中华田园人, Tony, kengyou_lei, LSSSS"
 description = locale == "zh" and "[版本: "..version..[[]
 
-更新内容: 优化文件结构.
+更新内容: 修复立绘显示问题.
 
 < 饥饿/精神/生命: 150/200/150 >
 < 攻击倍率: 75% >
@@ -18,7 +18,7 @@ description = locale == "zh" and "[版本: "..version..[[]
 or
 "[Version: "..version..[[]
 
-Changelog: modmain rework.
+Changelog: Fixed bigportraits display issue.
 
 < Hunger/Sanity/Health: 150/200/150 >
 < Damage: 75% >
