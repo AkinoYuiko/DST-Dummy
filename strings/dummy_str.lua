73eb83bae2ebaa83ddbc1357f5c4c1addbe1ad4f
Ice <xieshan4@gmail.com>
2021-07-01 01:50:58 +1000

    Format quotes

--- strings/dummy_str.lua
+++ strings/dummy_str.lua
@@ -1,40 +1,71 @@
 local STRINGS = GLOBAL.STRINGS
--- character
-STRINGS.NAMES.DUMMY = "Dummy"
-STRINGS.CHARACTER_NAMES.dummy = "Dummy"
-STRINGS.CHARACTER_TITLES.dummy = "The Nightmare Breaker"
-STRINGS.CHARACTER_ABOUTME.dummy = "Dummy also comes from the nightmare world. She is Mio's sister."
-STRINGS.CHARACTER_DESCRIPTIONS.dummy = "* Unfriendly to people.\n* Friend of nightmare.\n* Forgive."
-STRINGS.CHARACTER_QUOTES.dummy = "\"No one plays with me...\""
-STRINGS.CHARACTER_BIOS.dummy = {
-	{ title = "Birthday", desc = "July 17" },
-    { title = "Favorite Food", desc = STRINGS.NAMES.NIGHTMAREPIE },
-	-- { title = "Secret Knowledge", desc = "While toiling away in his home laboratory late one night, Wilson was startled to hear a voice on the radio speaking directly to him. At first he feared he'd gone mad from too many late nights of experiments and accidentally-inhaled chemical fumes, but the voice assured him that it was no mere figment of the imagination. In fact, the voice had a proposition for him: if Wilson would build a machine according to their specifications, then he would be rewarded with secret knowledge, the likes of which no one had ever seen. Casting aside his better judgement (after all, what harm could come from making a vague bargain with a mysterious disembodied voice?) Wilson threw himself into constructing the machine. When at long last it was finally completed, the gentleman scientist had a moment of hesitation... a moment that might have saved him from his impending fate, had he been just a bit stronger of will. But at the voice's insistence, Wilson flipped the switch and activated his creation... and was never seen again.\nWell, at least not in this world." },
+local strings = {
+	CHARACTER_ABOUTME = 
+	{
+		dummy = "Dummy also comes from the nightmare world. She is Mio's sister.",
+	},
+	CHARACTER_BIOS = 
+	{
+		dummy = {
+			{ title = "Birthday", desc = "July 17" },
+			{ title = "Favorite Food", desc = STRINGS.NAMES.NIGHTMAREPIE },
+		},
+	},
+	CHARACTER_DESCRIPTIONS = 
+	{
+		dummy = "* Unfriendly to people.\n* Friend of nightmare.\n* Forgive.",
+	},
+	CHARACTER_NAMES = 
+	{
+		dummy = "Dummy",
+	},
+	CHARACTER_QUOTES = 
+	{
+		dummy = "\"No one plays with me...\"",
+	},
+	CHARACTER_TITLES = 
+	{
+		dummy = "The Nightmare Breaker",
+	},
+	CHARACTERS = 
+	{
+		GENERIC = {
+			DESCRIBE = 
+			{
+				BLACKHOLESTAFF = "???",
+				BOOK_HARVEST = "H-A-R-V-E-S-T!",
+				BOOKTOGGLE_DOWNFALL = "/toggledownfall",
+				NIGHTMARE_SPEAR = "It looks like breaking.",
+			},
+		},
+	},
+	NAMES = 
+	{
+		BLACKHOLESTAFF = "Blackhole Staff",
+		BOOK_HARVEST = "Season of Harvest",
+		BOOK_TOGGLEDOWNFALL = "Love of Rain",
+		DUMMY = "Dummy",
+		NIGHTMARE_SPEAR = "Nightmare Spear",
+	},
+	RECIPE_DESC = 
+	{
+		BLACKHOLESTAFF = "Absorb everything!",
+		BOOK_HARVEST = "Should help you harvest!",
+		BOOK_TOGGLEDOWNFALL = "/toggledownfall",
+		NIGHTMARE_SPEAR = "Disappear in a moment.",
+	},
+	SKIN_DESCRIPTIONS = 
+	{
+		dummy_none = "Another girl comes from nightmare's world.",
+	},
+	SKIN_NAMES = 
+	{
+		dummy_none = "Dummy",
+		greenamulet_heart = "Green Heart",
+		raincoat_peggy = "Peggy's Raincoat",
+	},
+	TABS = 
+	{
+		["nightmaretab"] = "Nightmare"
+	}
 }
-
-STRINGS.SKIN_NAMES.dummy_none = "Dummy"
-STRINGS.SKIN_DESCRIPTIONS.dummy_none = "Another girl comes from nightmare's world."
-
--- recipes
-STRINGS.TABS["nightmaretab"] = "Nightmare"
-
-STRINGS.NAMES.BOOK_HARVEST = "Season of Harvest"
-STRINGS.RECIPE_DESC.BOOK_HARVEST = "Should help you harvest!"
-STRINGS.CHARACTERS.GENERIC.DESCRIBE.BOOK_HARVEST = "H-A-R-V-E-S-T!"
-
-STRINGS.NAMES.BOOK_TOGGLEDOWNFALL = "Love of Rain"
-STRINGS.RECIPE_DESC.BOOK_TOGGLEDOWNFALL = "/toggledownfall"
-STRINGS.CHARACTERS.GENERIC.DESCRIBE.BOOKTOGGLE_DOWNFALL = "/toggledownfall"
-
--- items
-STRINGS.NAMES.BLACKHOLESTAFF = "Blackhole Staff"
-STRINGS.RECIPE_DESC.BLACKHOLESTAFF = "Absorb everything!"
-STRINGS.CHARACTERS.GENERIC.DESCRIBE.BLACKHOLESTAFF = "???"
-
-STRINGS.NAMES.NIGHTMARE_SPEAR = "Nightmare Spear"
-STRINGS.RECIPE_DESC.NIGHTMARE_SPEAR = "Disappear in a moment."
-STRINGS.CHARACTERS.GENERIC.DESCRIBE.NIGHTMARE_SPEAR = "It looks like breaking."
-
-STRINGS.SKIN_NAMES.greenamulet_heart = "Green Heart"
-STRINGS.SKIN_NAMES.raincoat_peggy = "Peggy's Raincoat"
-
